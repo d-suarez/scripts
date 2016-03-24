710af8f1411adf6a9f7a95c3ca78bf78d764cde1
d-suarez <daniel@linuxmotors.com>
2016-03-23 19:13:51 -0500

    commit

--- extractaudio.sh
+++ extractaudio.sh
@@ -1,3 +1,19 @@
-#Check audio tracks with ffmpeg -i
+#!/bin/bash
 
-ffmpeg -i /input.mp4 -map 0:0 -map 0:1 -acodec copy -vcodec copy out.mp4 
+# ffmpeg script to extract audio tracks of a video file within a folder.
+OUT="/ram"
+
+for i in *
+do
+
+# Modify as needed. 
+ffmpeg -i "$i" "${i%.*}" -map 0:0 -map 0:1 -acodec copy -vcodec copy "$OUT" 
+done 
+
+
+
+
+
+
+
+
--- filebot.sh
+++ filebot.sh
@@ -1,1 +1,4 @@
-filebot -script fn:renall /Download/rtorrent/complete/ -non-strict --db TheMovieDB --output /stuff1/movies/720p\ Collection/ && filebot -script fn:cleaner /Download/rtorrent/complete/
+#!/bin/bash
+
+find /Download/movies/ -type f -name "*.srt" -exec rm -f {} \; && filebot -script fn:renall /Download/movies/Completed -non-strict --db TheMovieDB --output /Movies/720p\ Collection/ && filebot -script fn:cleaner /Download/movies/Completed && cd /Download/movies/Completed && rm -rf * && filebot -script fn:renall /Download/joy -non-strict --db TheMovieDB --output /Movies/1080p\ Collection/  && filebot -script fn:cleaner /Download/joy  && cd /Download/joy && rm -rf *
+
