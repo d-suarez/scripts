9dbc18e7567bf8d90258958006fb3f27eb649ed3
ds <d-suarez@users.noreply.github.com>
2016-03-23 19:09:23 -0500

    Delete filebot.sh

--- filebot.sh
+++
@@ -1,1 +0,0 @@
-filebot -script fn:renall /Download/rtorrent/complete/ -non-strict --db TheMovieDB --output /stuff1/movies/720p\ Collection/ && filebot -script fn:cleaner /Download/rtorrent/complete/
